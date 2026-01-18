583e724b9a0bc9ab893b035101c3cf144ad26bdf
Hans Sags <saguilayan.hansaldrich@gmail.com>
2026-01-18 06:32:04 +0000

    test.py

--- test.py
+++ test.py
@@ -9,10 +9,7 @@ def main():
     while True:
         print("\nChoose an operation:")
         print("1 - Add")
-        print("2 - Subtract")
-        print("3 - Multiply")
-        print("4 - Divide")
-        print("5 - Exit")
+       
 
         choice = input("Enter your choice: ")
 
